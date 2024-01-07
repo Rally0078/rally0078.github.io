f56e21268ef641152885b9e20196f596e911be12
Rally0078 <vimaljaideep009@gmail.com>
2024-01-07 23:35:58 +0530

    Edited portfolio website text in homepage

--- src/App.js
+++ src/App.js
@@ -10,16 +10,17 @@ function App() {
             <div class="navbar">
                 <nav>
                     <ul>
-                        <li class="hoverbutton"><a href="#">Home</a></li>
-                        <li class="hoverbutton"><a href="#">About</a></li>
-                        <li class="hoverbutton"><a href="#">Contact</a></li>
-                        <li class="hoverbutton"><a href="#">Links</a></li>
+                        <li class="hoverbutton"><a href="#" class="MenuHyper">About Me</a></li>
+                        <li class="hoverbutton"><a href="#" class="MenuHyper">Home</a></li>
+                        <li class="hoverbutton"><a href="#" class="MenuHyper">Contact</a></li>
                     </ul>
                 </nav>
             </div>
             <h3 class="pageTitle">Home Page</h3>
             <div class="content">
-                <p>Welcome to my Lily Garden. This is where I post pictures of flowers, particularly Lilies.</p>
+                <p>Welcome to my Lily Garden, this is my portfolio website. Feel free to look around and make nasty comments about it on my github.</p>
+                <h2 class="subheading">My Projects</h2>
+                <p></p>
             </div>
         </div>
     </div>
